fa67ab7a1504ab240a6c554851627ebc2bc52df1
apisit15 <645051200428@mail.rmutk.ac.th>
2024-01-24 11:57:13 +0700

    first commit

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:ze04/stylet.dart';
+import 'package:ze04/diceroll.dart';
 
 class GContainer extends StatelessWidget{
-    GContainer({super.key});
+  GContainer({super.key});
 
-    Widget build(context){
-      return Container(
+
+Widget build(context){
+    return Container( //พื้นหลังควรอยู่ด้านหลังตัวอักษร
         decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-          Color.fromARGB(221, 79, 209, 198),
-          Color.fromARGB(137, 165, 101, 165)
-          ],
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft)
-        ),
-        child:  Center(
-          child: StyleT()        
-      ));
-    }
+          gradient: LinearGradient(colors:[
+            Color.fromRGBO(255, 6, 69, 0.867),
+            Color.fromARGB(221, 9, 141, 250),
+            Color.fromARGB(221, 213, 227, 149)
+          ],begin:Alignment.topRight,end: Alignment.bottomLeft)),
+          child: Center( //ตั้งค่าตัวอักษรตรงกลาง
+            child: DiceThrown()));
+    
 }
+  }
--- lib/stylet.dart
+++ lib/stylet.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 class StyleT extends StatelessWidget{
-  StyleT({super.key});
+  StyleT(this.text,{super.key});
+
+  var text;
 
   Widget build(context){
-      return Text("Hello World",
-        style: TextStyle(fontSize: 48,
-        color: Colors.white));
-}
+     return Text(text, style: TextStyle(fontSize : 48, color: Colors.white));
+
+  
+    
+  }
 }
